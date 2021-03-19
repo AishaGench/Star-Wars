c9e39c3acece5b012e891f7b11564011b6ed9fa2
Aisha Gench <aisha.gench@gmail.com>
2021-03-19 17:33:46 -0500

    created function with parameter

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-
 const output = document.getElementById("output");
 const spinner = document.getElementById("spinner");
 const url = "https://starwars.egghead.training/";
+
+//Create a function
+function starWars(endpoint){
+    //use Api url
+    const url = 'https://starwars.egghead.training/' 
+
+}
 
