607882b0a81d8fffea47ae1e4f79b0a0144dce39
Aisha Gench <aisha.gench@gmail.com>
2021-03-19 20:46:16 -0500

    created chain

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ const url = "https://starwars.egghead.training/";
 //Create a function
 function starWars(endpoint){
     //used Api url
-    const url = 'https://starwars.egghead.training/' 
+    const url = 'https://starwars.egghead.training/' + endpoint;
+    return fetch(url).then((response)=>{
+        if(response.ok){
+            return response.json();
+        }else{
+            return Promise.reject(Error('Unsuccesful response'));
+        }
+    })
+    .then((data)=>{
+        console.log(data);
+        console.log(`There are ${data.length} ${endpoint}  in Starwars history`);
+        
 
+    }).catch((error)=>{
+        console.log(error);
+    })
 }
+starWars('films');
+
 
