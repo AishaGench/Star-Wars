f9dcfb8b9ea13ee0e996a68760651cd8735dbeb3
Aisha Gench <aisha.gench@gmail.com>
2021-03-23 00:26:33 -0500

    Created new button name of Change Star Wars

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const output = document.getElementById("output");
 const spinner = document.getElementById("spinner");
 const url = "https://starwars.egghead.training/";
+const btn = document.getElementById("button")
 
 //Create a function
 function starWars(endpoint){
@@ -16,12 +17,16 @@ function starWars(endpoint){
     .then((data)=>{
         console.log(data);
         console.log(`There are ${data.length} ${endpoint}  in Starwars history`);
+		ata.forEach(element => {console.log(`Film episode_${element.episode_id} title is ${element.title}`)});
         
 
     }).catch((error)=>{
         console.log(error);
     })
 }
+
+
+
 starWars('films');
 
 
